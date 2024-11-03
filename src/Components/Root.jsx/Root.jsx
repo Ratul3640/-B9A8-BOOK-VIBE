dbcfb72564c96299df656f9a84db4d50a40e41af
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-03 21:50:40 +0600

    banner section completed

--- src/Components/Root.jsx/Root.jsx
+++ src/Components/Root.jsx/Root.jsx
@@ -4,6 +4,9 @@ import { Outlet } from 'react-router-dom';
 const Root = () => {
     return (
         <div className='container mx-auto'>
+            <div>
+                
+            </div>
             <div className='min-h-[calc(100vh-80px)]'>
                 <Outlet></Outlet>
             </div>
