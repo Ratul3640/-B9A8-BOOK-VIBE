c42f261f7b5e6cc8dfab2452cd26733c19d139fa
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-04 02:05:27 +0600

    Navbar completed

--- src/Components/Root.jsx/Root.jsx
+++ src/Components/Root.jsx/Root.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 
 const Root = () => {
     return (
         <div className='container mx-auto'>
-            <div>
-                
+            <div className='h-20'>
+                <Navbar></Navbar>
             </div>
             <div className='min-h-[calc(100vh-80px)]'>
                 <Outlet></Outlet>
